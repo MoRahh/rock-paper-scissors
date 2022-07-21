7783eae5ded7d8ec2e20a53fd4658c1ce02c2606
MoRahh <moodyaburahma@gmail.com>
2022-07-21 17:16:23 +0300

    Changed outcome style

--- main.js
+++ main.js
@@ -18,35 +18,42 @@ const computerPlay = () => {
 
 
 const playRound = (playerSelection, computerSelection) => {
-    
+
     const p = document.createElement('p')
 
-    if(playerSelection === computerSelection) {
+    if (playerSelection === computerSelection) {
         p.innerText = `You tied! You both picked ${playerSelection}`
+        p.style.backgroundColor = "#2f2f2f";
     }
-    else if(playerSelection === 'scissors' && computerSelection === 'rock') {
+    else if (playerSelection === 'scissors' && computerSelection === 'rock') {
         compScore++
         p.innerText = 'You lost! Rock crushes scissors'
+        p.style.backgroundColor = "red";
     }
-    else if(playerSelection === 'scissors' && computerSelection === 'paper') {
+    else if (playerSelection === 'scissors' && computerSelection === 'paper') {
         playerScore++
         p.innerText = 'You Won! scissor cuts paper'
+        p.style.backgroundColor = "green";
     }
-    else if(playerSelection === 'rock' && computerSelection === 'paper') {
+    else if (playerSelection === 'rock' && computerSelection === 'paper') {
         compScore++
         p.innerText = 'You lost! paper covers rock'
+        p.style.backgroundColor = "red";
     }
-    else if(playerSelection === 'rock' && computerSelection === 'scissors') {
+    else if (playerSelection === 'rock' && computerSelection === 'scissors') {
         playerScore++
         p.innerText = 'You Won! Rock crushes scissors'
+        p.style.backgroundColor = "green";
     }
-    else if(playerSelection === 'paper' && computerSelection === 'scissors') {
+    else if (playerSelection === 'paper' && computerSelection === 'scissors') {
         compScore++
         p.innerText = 'You lost! scissors cuts paper'
+        p.style.backgroundColor = "red";
     }
-    else if(playerSelection === 'paper' && computerSelection === 'rock') {
+    else if (playerSelection === 'paper' && computerSelection === 'rock') {
         playerScore++
         p.innerText = 'You won! paper covers rock'
+        p.style.backgroundColor = "green";
     }
     outcome.appendChild(p);
 }
@@ -54,17 +61,21 @@ const playRound = (playerSelection, computerSelection) => {
 
 
 const checkForWinner = (playerScore, compScore) => {
-    if(playerScore === 5){
+    if (playerScore === 5) {
         const h2 = document.createElement('h2');
         h2.classList.add('player-won')
-        h2.style.cssText = "color:green;"
+        h2.style.cssText = "color:white;"
+        h2.style.backgroundColor = "green";
+        h2.style.padding = "1%";
         h2.innerText = `You won ${playerScore} to ${compScore} great job beating the computer.`
         outcome.append(h2)
     }
     if (compScore === 5) {
         const h2 = document.createElement('h2');
         h2.classList.add('computer-won')
-        h2.style.cssText = "color:red;"
+        h2.style.cssText = "color:white;"
+        h2.style.backgroundColor = "red";
+        h2.style.padding = "1%";
         h2.innerText = `You lost ${playerScore} to ${compScore} great job beating the computer.`
         outcome.append(h2)
     }
@@ -81,7 +92,7 @@ rockButton.addEventListener('click', () => {
     const computerSelection = computerPlay();
     const playerSelection = 'rock';
     playRound(playerSelection, computerSelection);
-    updateScores(playerScore,compScore)
+    updateScores(playerScore, compScore)
     checkForWinner(playerScore, compScore)
 });
 
@@ -89,7 +100,7 @@ paperButton.addEventListener('click', () => {
     const computerSelection = computerPlay();
     const playerSelection = 'paper';
     playRound(playerSelection, computerSelection);
-    updateScores(playerScore,compScore)
+    updateScores(playerScore, compScore)
     checkForWinner(playerScore, compScore)
 });
 
@@ -97,6 +108,6 @@ scissorsButton.addEventListener('click', () => {
     const computerSelection = computerPlay();
     const playerSelection = 'scissors';
     playRound(playerSelection, computerSelection);
-    updateScores(playerScore,compScore)
+    updateScores(playerScore, compScore)
     checkForWinner(playerScore, compScore)
 });
